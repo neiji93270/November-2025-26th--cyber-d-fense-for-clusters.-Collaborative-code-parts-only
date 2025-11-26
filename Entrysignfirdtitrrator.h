5df38a920ab6cb653434602c01030a1b51329e61
Altinisik nejdet <nejdet.altinisik@yahoo.fr>
2025-11-26 07:49:12 +0100

    Create Entrysignfirdtitrrator.h

---
+++ Entrysignfirdtitrrator.h
@@ -0,0 +1,7 @@
+//there are propertied of lie groups
+//that are not suited even with signing with pro p groupe
+//if the amount of functional formed atomic and
+//langage based componentz is not clzmped to get an attack from outside the lie
+//groups eelated it behaviours and if the attackers dont know the usage of internal
+//chaotic and functionzl systems. this is a rough idea but it is good to have a peotocol formed
+//by mathz knowledged. after that, we dont havr to set any algorithms.
